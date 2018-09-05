814acb84d754e34c53b714de752d441a98e8a25e
rvsdcool <42895674+rvsdcool@users.noreply.github.com>
2018-09-05 23:11:19 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
-path_to_zip_file = "datasets.zip"
-directory_to_extract_to = "./11/"
+path_to_zip_file = "datasets.zip" #you can write name of the zip file
+directory_to_extract_to = "./11/" # put "" to if the zip file and extracted filein the same directory or else put the location 
 import zipfile
 zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
 zip_ref.extractall(directory_to_extract_to)
 zip_ref.close()
+#save this file as .py format and execute
